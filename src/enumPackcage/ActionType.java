8dc145457591e99d0529fed0cb20561be755b23f
2dam <2dam@HZ375747.TartangaLH.eus>
2022-10-25 11:59:15 +0200

    Enumerations create

---
+++ src/enumPackcage/ActionType.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enumPackcage;
+
+/**
+ * Esta e la calse enumeracion que usaremos para determinar el tipo de accion.
+ * @author iker
+ */
+public enum ActionType {
+    SIGNUP,SIGNIN
+}
---
+++ src/enumPackcage/ExceptionType.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enumPackcage;
+
+/**
+ * Esta clase es la enumeracion que determina el tipo de excepcion.
+ * @author iker
+ */
+public enum ExceptionType {
+    LOGINUSERNAMEEXCEPTION,
+    LOGINPASSWORDEXCEPTION,
+    LOGINUSERNAMEANDPASSWORDEXCEPTION,
+    SIGNUPUSERNAMEEXCEPTION,
+    SIGNUPEMAILEXCEPTION,
+    SIGNUPEMAILANDUSERNAMEEXCEPTION,
+    SERVERCONECTIONEXCEPTION
+
+}

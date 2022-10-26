76aa93b2a52b06db5a8008c263cb512f7b6fa023
2dam <2dam@HZ375747.TartangaLH.eus>
2022-10-26 13:30:19 +0200

    message create 1.1

--- src/messagePackage/Message.java
+++ src/messagePackage/Message.java
@@ -19,10 +19,39 @@ public class Message {
     private ActionType acType;
     private ExceptionType exType;
     
+    public Message(){
+        
+    }
+    
     public Message(User usuario,ActionType aType, ExceptionType eType){
         this.usu = usuario;
         this.acType = aType;
         this.exType = eType;
     }
+
+    public User getUsu() {
+        return usu;
+    }
+
+    public void setUsu(User usu) {
+        this.usu = usu;
+    }
+
+    public ActionType getAcType() {
+        return acType;
+    }
+
+    public void setAcType(ActionType acType) {
+        this.acType = acType;
+    }
+
+    public ExceptionType getExType() {
+        return exType;
+    }
+
+    public void setExType(ExceptionType exType) {
+        this.exType = exType;
+    }
+    
     
 }
